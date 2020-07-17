9407989a89cdd51addb7754e16b7bdb1f484e76f
Roderick De Groen <roderickdegroen@MacBook-Pro-van-Roderick.local>
2020-07-17 14:32:30 +0200

    patch

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,7 +4,6 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
-import Testimonials from '../components/Testimonials'
 
 export const IndexPageTemplate = ({
   image,
@@ -84,7 +83,6 @@ export const IndexPageTemplate = ({
           </div>
         </div>
       </div>
-      <Testimonials />
 
     </section>
   </div>
